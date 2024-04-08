5be9d288f32fccb619c527fb8cd61e3aabbf66e9
ZunLise <2x2math@gmail.com>
2024-04-08 14:11:53 +0300

    tested 2 triangle functions

---
+++ Yermakov_simplemath/simplemath_tests/triangle_tests.py
@@ -0,0 +1,9 @@
+import Yermakov_simplemath
+from Yermakov_simplemath.Yermakov_simplemath.simplemath import *
+
+print(triangle_isright(3, 4, 5))
+print(triangle_isright(3, 4, 6))
+print(triangle_isright(3, 4, 4))
+
+print(triangle_area(3, 4, 5))
+print(triangle_area(3, 4, 8))
